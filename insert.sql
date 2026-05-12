dc309c3cf4601450c1b98cda1541826346a85b69
Vinícius Leite <contato.vinips25@outlook.com>
2026-05-12 10:09:03 -0300

    chore(US-01): Arrumando o insert agora funciona

--- insert.sql
+++
@@ -1,7 +0,0 @@
-USE db_kernelpanic;
-
-INSERT INTO projetos (nome, descricao, status, prazo)
-VALUES
-  ('Site Institucional', 'Criação do site da empresa', 'EM_ANDAMENTO', '2025-12-31 18:00:00'),
-  ('App Mobile', 'Desenvolvimento do app Android e iOS', 'EM_PLANEJAMENTO', '2026-03-01 18:00:00'),
-  ('Sistema ERP', 'Implantação do ERP interno', 'CONCLUIDO', '2024-06-30 18:00:00');
---
+++ projeto-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO projetos (nome, descricao, status, prazo, data_criacao)
+VALUES
+  ('Site Institucional', 'Criação do site da empresa', 'EM_ANDAMENTO', '2025-12-31 18:00:00', NOW()),
+  ('App Mobile', 'Desenvolvimento do app Android e iOS', 'EM_PLANEJAMENTO', '2026-03-01 18:00:00', NOW()),
+  ('Sistema ERP', 'Implantação do ERP interno', 'CONCLUIDO', '2024-06-30 18:00:00', NOW());
