aac812d2e27e663fc42f74e2c2a9f75e3d2f6706
Vinícius Leite <contato.vinips25@outlook.com>
2026-05-12 07:52:10 -0300

    feat(US-01): Implementando cors no service de projeto

--- projeto-service/src/main/java/com/kernelpanic/projeto_service/excecoes/GlobalExceptionHandler.java
+++ projeto-service/src/main/java/com/kernelpanic/projeto_service/excecoes/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.kernelpanic.projeto_service.excecoes;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
