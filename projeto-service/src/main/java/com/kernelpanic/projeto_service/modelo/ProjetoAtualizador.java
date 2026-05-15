bcacd59851e41e91c0f2b8a63d115bdf0c2b5e2b
Vinícius Leite <contato.vinips25@outlook.com>
2026-05-14 22:06:42 -0300

    fix(US-01): Arrumando o atualizador do projeto, agora ele pega o status,responsaveis e profissionais

--- projeto-service/src/main/java/com/kernelpanic/projeto_service/modelo/ProjetoAtualizador.java
+++ projeto-service/src/main/java/com/kernelpanic/projeto_service/modelo/ProjetoAtualizador.java
@@ -1,27 +1,38 @@
 package com.kernelpanic.projeto_service.modelo;
 
 import org.springframework.stereotype.Component;
-
 import com.kernelpanic.projeto_service.entidades.Projeto;
 
 @Component
 public class ProjetoAtualizador {
-    
+
     public void atualizar(Projeto projetoAtual, Projeto atualizacao) {
         if (atualizacao.getNome() != null && !atualizacao.getNome().isEmpty()) {
             projetoAtual.setNome(atualizacao.getNome());
         }
-        
+
         if (atualizacao.getDescricao() != null && !atualizacao.getDescricao().isEmpty()) {
             projetoAtual.setDescricao(atualizacao.getDescricao());
         }
-        
+
+        if (atualizacao.getStatus() != null) {
+            projetoAtual.setStatus(atualizacao.getStatus());
+        }
+
         if (atualizacao.getPrazo() != null) {
             projetoAtual.setPrazo(atualizacao.getPrazo());
         }
 
         if (atualizacao.getValorContratado() != null) {
             projetoAtual.setValorContratado(atualizacao.getValorContratado());
+        }
+
+        if (atualizacao.getResponsavelId() != null) {
+            projetoAtual.setResponsavelId(atualizacao.getResponsavelId());
+        }
+
+        if (atualizacao.getProfissionaisIds() != null) {
+            projetoAtual.setProfissionaisIds(atualizacao.getProfissionaisIds());
         }
     }
 }
