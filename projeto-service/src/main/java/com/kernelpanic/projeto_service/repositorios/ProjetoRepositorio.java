658f0e76e12742b6d6bec921c6f45814a177e3af
Tomio <miguelnonaka@gmail.com>
2026-05-29 20:28:59 -0300

    Update ProjetoRepositorio.java

--- projeto-service/src/main/java/com/kernelpanic/projeto_service/repositorios/ProjetoRepositorio.java
+++ projeto-service/src/main/java/com/kernelpanic/projeto_service/repositorios/ProjetoRepositorio.java
@@ -1,20 +1,23 @@
 package com.kernelpanic.projeto_service.repositorios;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import com.kernelpanic.projeto_service.entidades.Projeto;
 
 public interface ProjetoRepositorio extends JpaRepository<Projeto, Long> {
     
     @Query("SELECT p FROM Projeto p WHERE " +
-           "FUNCTION('JSON_CONTAINS', p.profissionaisIds, CAST(:profissionalId AS string)) = true OR " +
+           "FUNCTION('JSON_CONTAINS', p.profissionaisIds, CAST(:profissionalId AS string)) = 1 OR " +
            "p.responsavelId = :profissionalId")
     List<Projeto> findByProfissionalId(@Param("profissionalId") Long profissionalId);
     
     @Query("SELECT p FROM Projeto p WHERE " +
-           "(FUNCTION('JSON_CONTAINS', p.profissionaisIds, CAST(:profissionalId AS string)) = true OR " +
+           "(FUNCTION('JSON_CONTAINS', p.profissionaisIds, CAST(:profissionalId AS string)) = 1 OR " +
            "p.responsavelId = :profissionalId) AND " +
            "(:dataInicio IS NULL OR p.dataCriacao >= :dataInicio) AND " +
            "(:dataFim IS NULL OR p.prazo <= :dataFim)")
